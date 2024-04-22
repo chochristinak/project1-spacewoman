3892755080405331a88260a681027bec7a239b90
chock1991 <chochristinak@gmail.com>
2024-04-22 04:30:47 -0600

    Add keyup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
+window.onload = function() {
+  document.getElementById("insert-letter").focus();
+};
 const HINTS = ["energy centers of the body", "opening of the third eye", "a state of deep immersion and focus", "bend so you dont break", "tranquil", "the deep blue abyss", "mother nature", "a creative style of yoga that connects breath and movement", "dark and feminine principle of the universe", "spore-bearing fruit, some might call breakthrough medicine"]
 
-const WORDS = ["shakra", "enlightenment", "flow", "stretch", "calm", "ocean", "earth", "vinyasa", "yin", "psilocybin"] 
+const WORDS = ["shakra", "enlightenment", "flow", "stretch", "calm", "ocean", "earth", "vinyasa", "yin", "psilocybin"]
 let randWordIdx = Math.floor(Math.random() * WORDS.length);
 let chosenWord = WORDS[randWordIdx];
 let displayHint = HINTS[randWordIdx];
@@ -9,20 +12,27 @@ document.querySelector('.hint').innerHTML = "Hint: " + displayHint
 
 let holdList = [];
 let numGuesses = (chosenWord.length) + 1
-let typedLetter = document.getElementById("insert-letter"); 
-let displayWord = document.getElementById("displayWord"); 
-let placeHolder = document.getElementById("placeholders"); 
-let guessesLeft = document.getElementById("guesscount"); 
+let typedLetter = document.getElementById("insert-letter");
+let displayWord = document.getElementById("displayWord");
+let placeHolder = document.getElementById("placeholders");
+let guessesLeft = document.getElementById("guesscount");
 let tryLetterButton = document.getElementById("try");
-let correctGuess = document.getElementById("good-job");
-let tryAgain = document.getElementById("try-again");
 let winAlert = document.getElementById("winAlert");
 let chosenWordArr = new Array(chosenWord.length).fill("_");
 let messageEl = document.querySelector("h3");
 let letter
+let image = new Image();
+image.src = "https://totemsurftribe.files.wordpress.com/2024/02/img_1983-2.jpg?resize=438%2C438";
+let canvas = document.getElementById("spacewoman");
+let ctx = canvas.getContext("2d");
 
 
 tryLetterButton.addEventListener('click', updateDisplay);
+typedLetter.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    updateDisplay();
+  }
+});
 
 
 updateDisplay();
@@ -53,6 +63,9 @@ function updateDisplay() {
   correctPlacement()
   typedLetter.value = "";
   displayWordWithUnderscores();
+  drawImage();
+  typedLetter.focus();
+  
 }
 
 function correctPlacement() {
@@ -66,7 +79,13 @@ function correctPlacement() {
 }
 
 function letterGuess() {
-  if (numGuesses === 0) {
+
+    if (!chosenWord.includes(letter)) {
+      numGuesses--;
+      guessesLeft.innerText = `Guesses left: ${numGuesses}`;
+    }
+
+   if (numGuesses === 0) {
     {
       messageEl.innerText = "Oh no, you're out of guesses. The word was: " + `${chosenWord.toUpperCase()}`;
       document.getElementById("try").disabled = true;
@@ -80,10 +99,17 @@ function letterGuess() {
   }
 
   if (!chosenWord.includes(letter)) {
-    numGuesses--;
+    numGuesses+= 0;
     guessesLeft.innerText = `Guesses left: ${numGuesses}`;
   }
 }
+
+// function drawImage() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+//   ctx.globalAlpha = numGuesses / chosenWord.length; // Set the opacity
+//   ctx.drawImage(image, 0, 0, canvas.width, canvas.height); // Draw the image
+// }
+
 function win() {
   if (chosenWordArr.join("") === chosenWord) {
     winAlert.innerText = "Namaste. You successfully guessed the word."
